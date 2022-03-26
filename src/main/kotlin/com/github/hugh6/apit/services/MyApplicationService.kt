52d4c8b70d93d84ac0fd7bfca720dbdcfb259f23
GitHub Action <action@github.com>
2022-03-26 14:46:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/hugh6/apit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hugh6/apit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hugh6.apit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hugh6.apit.MyBundle
 
 class MyApplicationService {
 
